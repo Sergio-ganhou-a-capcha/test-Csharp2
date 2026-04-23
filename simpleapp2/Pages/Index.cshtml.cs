0de6449f5eeb3db05562f426843e6c5a4224be8d
Sergio-ganhou-a-capcha <a14618@oficina.pt>
2026-04-23 11:50:24 +0000

    sssss

---
+++ simpleapp2/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using MyCoreApp.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MyCoreApp.Pages;
+
+public class IndexModel : PageModel{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public IndexModel(IHttpClientFactory httpClientFactory){
+        _httpClientFactory = httpClientFactory;
+        }
+    public List<Pais> Paises { get; set; } = new();
+
+    public async Task OnGetAsync(){
+        //var client = _httpClientFactory.CreateClient();
+        //var response = await client.GetAsync("https://restcountries.com/v3.1/all");
+
+        var client = _httpClientFactory.CreateClient("RestCountries");
+        var response = await client.GetAsync("v3.1/all");
+
+        if (response.IsSuccessStatusCode){
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var dados = JsonSerializer.Deserialize<List<CountryApiResponse>>(json, options);
+
+            Paises = dados.Select(d => new Pais{
+                OfficialName = d.name?.official,
+                Cca2 = d.cca2,
+                FlagUrl = d.flags?.png
+            }).ToList();
+        }
+    }
+    /*
+private readonly ILogger<IndexModel> _logger;
+    public IndexModel(ILogger<IndexModel> logger){
+        _logger = logger;
+    }
+    public void OnGet(){
+
+    }
+*/
+}
---
+++ simpleapp2/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddRazorPages();
+
+
+/** pedaço de codigo inicio */
+
+//builder.Services.AddHttpClient();
+
+builder.Services.AddHttpClient("RestCountries", c =>
+
+{
+
+    c.BaseAddress = new Uri("https://restcountries.com/");
+
+})
+
+.ConfigurePrimaryHttpMessageHandler(() =>
+
+{
+
+    return new HttpClientHandler
+
+    {
+
+        SslProtocols = System.Security.Authentication.SslProtocols.Tls12
+
+    };
+
+});
+
+/** pedaço de codigo fim*/
+
+
+var app = builder.Build();
